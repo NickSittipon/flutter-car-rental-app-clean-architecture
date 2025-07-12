c1a375f18acaede699cf1e7e9a8142f63853c40a
NickSittipon <sittipon5725@gmail.com>
2025-07-12 20:46:19 +0700

    feat(ui): add onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_car_rental_app_clean_architecture/onboarding_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -19,70 +20,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: OnboardingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  FirebaseFirestore db = FirebaseFirestore.instance;
-  int _counter = 0;
-
-  void _incrementCounter() {
-    final user = {
-      'firstname': 'John',
-      'lastname': "danny",
-      'born': 0
-    };
-    db.collection("user").add(user).then((DocumentReference doc) {
-      print("DocumentSnapshot added with ID: ${doc.id}");
-    }).catchError((error) {
-      print("Error adding document: $error");
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
- 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
