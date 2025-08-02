cef754bf7629831b4659ddae981f7b7ab65a57c2
NickSittipon <sittipon5725@gmail.com>
2025-08-03 00:15:25 +0700

    feat: integrate BLoC for state management

---
+++ lib/presentation/bloc/car_state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_car_rental_app_clean_architecture/data/model/car.dart';
+
+abstract class CarState {}
+
+class CarsLoading extends CarState {}
+
+class CarsLoaded extends CarState {
+  final List<Car> cars;
+  CarsLoaded(this.cars);
+}
+
+class CarsError extends CarState {
+  final String message;
+  CarsError(this.message);
+}
